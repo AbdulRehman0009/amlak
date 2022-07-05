5ddd1a656989c33f971300f7faaff613a636f2d8
AbdulRehman0009 <abdulrehmanmw8@gmail.com>
2022-07-05 03:48:05 -0700

    new project

---
+++ property/jawa.js
@@ -0,0 +1,232 @@
+function function1(){
+  var element = document.getElementById("1");
+  var element2 = document.getElementById("2");
+  var element3 = document.getElementById("b9");
+  var element4 = document.getElementById("b10");
+ element2.classList.remove("button1");
+ element2.classList.add("button2");
+ element.classList.remove("button2");
+ element.classList.add("button1");
+ element3.style.display='block';
+ element4.style.display='none';
+ 
+}
+function function2(){
+    var element = document.getElementById("1");
+    var element2 = document.getElementById("2");
+    var element3 = document.getElementById("b9");
+    var element4 = document.getElementById("b10");
+   element.classList.remove("button1");
+   element.classList.add("button2");
+   element2.classList.add("button1");
+   element2.classList.remove("button2");
+   element3.style.display='none';
+   element4.style.display='block';
+
+}
+
+function function3() {
+
+    document.getElementById("3").style.display='none';
+    document.getElementById("4").style.display='flex';
+    document.getElementById("8").style.display='none';
+   
+    document.getElementById("b1").style.display='none';
+    document.getElementById("b2").style.display='block';
+  }
+
+  function function4() {
+    document.getElementById("4").style.display='none';
+    document.getElementById("3").style.display='flex';
+    document.getElementById("8").style.display='flex';
+    
+    document.getElementById("b1").style.display='block';
+    document.getElementById("b2").style.display='none';
+    
+    
+  }
+
+function function5() {
+    document.getElementById("5").style.display='none';
+    document.getElementById("6").style.display='block';
+    document.getElementById("7").style.display='flex';
+}  
+function function6() {
+  document.getElementById("6").style.display='none';
+    document.getElementById("5").style.display='block';
+    document.getElementById("7").style.display='none';
+}  
+function function7() {
+  document.getElementById("villa").style.height='100%';
+  document.getElementById("style2").style.display='block';
+  document.getElementById("style1").style.display='none';
+}  
+function function8() {
+  document.getElementById("villa").style.height='70px';
+  document.getElementById("style2").style.display='none';
+  document.getElementById("style1").style.display='block';
+}  
+
+    $('#carousel-1').carousel({
+      interval: 4000,
+      wrap: true,
+      keyboard: true
+ });
+ 
+ 
+ /* 2 carousel */
+ $('#carousel-2').carousel({
+      interval: 6000,
+      wrap: true,
+      keyboard: true
+ });
+ 
+ 
+ /* 3 carousel */
+ $('#carousel-3').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 4 carousel example with jumbotron */
+ $('#carousel-4').carousel({
+     interval: 10000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 5 carousel example */
+ $('#carousel-5').carousel({
+     interval: 6000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 6 carousel example */
+ $('#carousel-6').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 7 carousel example */
+ $('#carousel-7').carousel({
+     interval: 4000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 8 carousel example */
+ $('#carousel-8').carousel({
+     interval: 6000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 9 carousel example */
+ $('#carousel-9').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 10 carousel example */
+ $('#carousel-10').carousel({
+     interval: 2000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 11 carousel example */
+ $('#carousel-11').carousel({
+     interval: 4000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 12 carousel example */
+ $('#carousel-12').carousel({
+     interval: 6000,
+     wrap: true,
+     keyboard: true
+ });
+
+ /* 13 carousel example */
+ $('#carousel-13').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
+
+ $('#carousel-14').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+$('#carousel-15').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+$('#carousel-16').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+ $('#carousel-17').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
+ 
+ $('#carousel-18').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+ $('#carousel-19').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
+ 
+ $('#carousel-20').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+$('#carousel-21').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+$('#carousel-22').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+$('#carousel-23').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+$('#carousel-24').carousel({
+    interval: 8000,
+    wrap: true,
+    keyboard: true
+});
+
+ $('#carousel-25').carousel({
+     interval: 8000,
+     wrap: true,
+     keyboard: true
+ });
